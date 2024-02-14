0ff98b19e6b7ced67e320f0674356b7e22bba50c
Mai <maiabdelhameed16@gmail.com>
2024-02-14 18:14:39 +0200

    initial

--- parser/Token.py
+++
@@ -1,9 +0,0 @@
-class Token:
-
-    def __init__(self, Type, name=None, start=None, end=None, size = None):
-        self.Type = Type
-        self.name = name
-        self.start = start
-        self.end = end
-        self.size = size
-        
--- parser/preprocessing/extract.py
+++
@@ -1,6 +0,0 @@
-from find import get_input_output
-import requests
-
-file = open("module.txt")
-
-print(get_input_output(file))
