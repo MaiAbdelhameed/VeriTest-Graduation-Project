31f218ff0e901f25de884928f4aff6d7b2c22d4b
Mai <maiabdelhameed16@gmail.com>
2024-03-09 18:38:15 +0200

    synthesis done

--- ALL_COMBINATIONAL/success/d3.v
+++
@@ -1,9 +0,0 @@
-module d3 (input [1:0] a,input [1:0] b,
-                    output reg [1:0] quotient, output reg [1:0] remainder);
-
-
-    always @(*) begin
-        quotient = a / b;
-        remainder = a % b;
-    end
-endmodule
